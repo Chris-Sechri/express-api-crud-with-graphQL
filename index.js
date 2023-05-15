35feaab24f4268f18fd571ec499084704ea16453
Chris LOUNKANA <chris.lounkana.p@gmail.com>
2023-05-15 00:02:38 +0000

    build of first graphQl API

---
+++ index.js
@@ -0,0 +1,62 @@
+const { ApolloServer, gql } = require('apollo-server');
+const {
+    ApolloServerPluginLandingPageLocalDefault
+  } = require('apollo-server-core');
+  
+const typeDefs = gql`
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+  }
+`;
+
+
+const books = [
+    {
+      title: 'The Awakening',
+      author: 'Kate Chopin',
+    },
+    {
+      title: 'City of Glass',
+      author: 'Paul Auster',
+    },
+  ];
+
+const resolvers = {
+    Query: {
+      books: () => books,
+    },
+  };
+
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    csrfPrevention: true,
+    cache: 'bounded',
+    /**
+     * What's up with this embed: true option?
+     * These are our recommended settings for using AS;
+     * they aren't the defaults in AS3 for backwards-compatibility reasons but
+     * will be the defaults in AS4. For production environments, use
+     * ApolloServerPluginLandingPageProductionDefault instead.
+    **/
+    plugins: [
+      ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+    ],
+  });
+
+
+  server.listen().then(({ url }) => {
+      console.log(`Server is running on ${url}`);
+  })
